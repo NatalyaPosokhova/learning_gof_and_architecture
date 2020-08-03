82165e87888659800410c1cf0dc4f4463ccdfc7d
Natalya Posokhova <Natalya.Posokhova@emerson.com>
2020-08-03 23:14:44 +0500

    Lesson1

---
+++ SeaWar/src/SeaWar/Ship.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeaWar
+{
+    /// <summary>
+    /// Implement Ship
+    /// </summary>
+    public class Ship
+    {
+        //private int _deck;
+        //int DeckQuantity
+        //{
+        //    get
+        //    {
+        //        return _deck;
+        //    }
+        //    set
+        //    {
+        //        _deck = value;
+        //    }
+        //}
+        /// <summary>
+        /// Returns Deck quantity
+        /// </summary>
+        public int DeckQuantity { get; private set; }
+        /// <summary>
+        /// Initial ship coordinates
+        /// </summary>
+        public Point Position { get; private set; }
+        /// <summary>
+        /// Determines the ship state.
+        /// </summary>
+        public ShipState State { get; private set; }
+        /// <summary>
+        /// Determines the ship direction.
+        /// </summary>
+        public ShipDirection Direction { get; private set; }
+        /// <summary>
+        /// Checks if the shot hits the ship.
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public bool CheckShooting(Point point)
+        {
+            throw new NotImplementedException();
+        }
+        public Ship()
+        {
+
+        }
+    }
+
+    public enum ShipState {Live, Died, Injured};
+    public enum ShipDirection { Vertical, Horizontal};
+    public struct Point
+    {
+        public int x;
+        public int y;
+    }
+}
