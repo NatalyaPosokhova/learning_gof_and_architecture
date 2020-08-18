722d6a2333957d8432f80c0e144c52e430fec4ae
Natalya Posokhova <NatalyaPosokhova@gmail.com>
2020-08-19 01:29:26 +0500

    CheckRuleCapabilities method was finished

--- src/SeaWar/Board.cs
+++ src/SeaWar/Board.cs
@@ -7,6 +7,33 @@ namespace SeaWar
 {
     public class Board
     {
-        
+        private const int boardSize = 10;
+        private List<Ship> shipsList = new List<Ship>();
+        public bool CheckRuleCapabilities(Point point, ShipDirection direction, int deckQuantity)
+        {
+            bool result = true;
+            if(direction == ShipDirection.Vertical)
+            {
+                if((point.y + deckQuantity) >= boardSize)
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                if((point.x + deckQuantity) >= boardSize)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+        public void CreateShip(Point point, ShipDirection direction, int deckQuantity)
+        {
+            if(!CheckRuleCapabilities(point, direction, deckQuantity))
+            {
+                throw new CreateShipException();
+            }
+        }
     }
 }
