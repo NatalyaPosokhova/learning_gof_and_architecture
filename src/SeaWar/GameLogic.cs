c6c844058b380d91ed84ee854e9de423a2f6d551
Natalya Posokhova <NatalyaPosokhova@gmail.com>
2020-08-20 22:22:17 +0500

    GameLogic Class was added

---
+++ src/SeaWar/GameLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeaWar
+{
+    public class GameLogic
+    {
+        private List<Player> _players;
+        private List<Board> _boards;
+        public GameLogic(List<Player> Players, List<Board> Boards)
+        {
+            _players = Players;
+            _boards = Boards;
+        }
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
